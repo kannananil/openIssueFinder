fbc8e6af681dce05deba45d000bbdf0809e1d10c
anil-muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2022-11-09 01:02:57 +0530

    [ Anil ] | Updated config with github related env variables

--- bff/src/config/env.js
+++ bff/src/config/env.js
@@ -4,4 +4,6 @@ dotenv.config({path:`${process.env.PWD}/.env`});
 
 module.exports = {
   PORT: process.env.PORT || '3003',
+  GITHUB_API_BASE_URL: process.env.GITHUB_API_BASE_URL,
+  GITHUB_API_TOKEN: process.env.GITHUB_API_TOKEN,
 };
