f99a96cf799edd08bc9c122e9cfa9d60087fbd28
anil-muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2022-11-09 01:00:13 +0530

    [ Anil ] | Added a handler method to search repositories based on query

---
+++ bff/src/api/handlers/github.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const env = require('../../config/env');
+
+const formatQuery = (queries) => Object.entries(queries).map(([ key, value ]) => `${key}:${value}`).join(' ');
+
+const searchRepositories = async (req, res) => {
+  const baseURL = env.GITHUB_API_BASE_URL;
+  const token = env.GITHUB_API_TOKEN;
+  const query = encodeURIComponent(formatQuery({topic: 'hacktoberfest', language: 'javascript'}));
+  const url = `${baseURL}/search/repositories?q=${query}`;
+  const requestConfig = {
+    method: 'get',
+    url: url,
+    headers: { Authorization: `token ${token}`, 'Content-Type': 'application/json' },
+  };
+  axios(requestConfig)
+    .then((response) => res.json(response.data))
+    .catch((error) => res.status(500).json(error));
+};
+
+module.exports = { searchRepositories };
