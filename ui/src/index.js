ee415f5c1521c7d0bd071976d69e2cd56b13af68
anil-muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2022-11-29 21:39:07 +0530

    [ Anil ] | Dark theme implementation using material UI

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 import App from './App';
+
+
+const darkTheme = createTheme({
+  palette: { mode: 'dark' }
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
