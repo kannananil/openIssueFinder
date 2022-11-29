59f6152f9a5a7af5577085905f644bf070a4086e
anil-muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2022-11-29 21:41:06 +0530

    [ Anil ] | Basic repo card design implementation

--- ui/src/components/Repos/index.js
+++ ui/src/components/Repos/index.js
@@ -1,15 +1,22 @@
+import { Button, Card, CardContent, Typography } from '@mui/material';
 import useReposController from '../../redux/controller/repos';
+import './index.css';
 
 const Repos = () => {
   const reposData = useReposController();
 
   const RepoList = () => {
     const repos = reposData?.value?.items?.map((repo) => (
-      <div>
-        <p>{repo.full_name}</p>
-      </div>
+      <Card sx={{ width: 350, height: 200 }} className='card'>
+        <CardContent>
+          <Typography className='card-subtitle' color="text.secondary" gutterBottom>{repo.full_name}</Typography>
+          <Typography className='card-title' gutterBottom>{repo?.name}</Typography>
+          <Typography className='card-description' color="text.secondary" gutterBottom>{repo.description}</Typography>
+          <Button href={repo.html_url} variant='outlined' size='small'>Open Repo</Button>
+        </CardContent>
+      </Card>
     ))
-    return <div>{repos}</div>
+    return <div className='cards'>{repos}</div>
   }
 
   return reposData?.value?.items ? (<RepoList/>) : (<div>Repos getting loaded!</div>);
